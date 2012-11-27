9088bc30f8d59f5d3c6bfa243ea6bec57e0935f0
SPolischook <spolischook@gmail.com>
2012-11-28 01:38:49 +0200

    Install pagerfanta

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Serge\GuestbookBundle\GuestbookBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
